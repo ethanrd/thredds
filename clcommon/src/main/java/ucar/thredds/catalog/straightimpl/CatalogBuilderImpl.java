c4923af151237c9b4211f79987ba9ca636ff7c25
Ethan Davis <edavis@unidata.ucar.edu>
2014-01-27 17:03:05 -0700

    Start "fresh" in clcommon with package ucar.thredds.catalog

---
+++ clcommon/src/main/java/ucar/thredds/catalog/straightimpl/CatalogBuilderImpl.java
@@ -0,0 +1,277 @@
+/*
+ * Copyright 1998-2009 University Corporation for Atmospheric Research/Unidata
+ *
+ * Portions of this software were developed by the Unidata Program at the
+ * University Corporation for Atmospheric Research.
+ *
+ * Access and use of this software shall impose the following obligations
+ * and understandings on the user. The user is granted the right, without
+ * any fee or cost, to use, copy, modify, alter, enhance and distribute
+ * this software, and any derivative works thereof, and its supporting
+ * documentation for any purpose whatsoever, provided that this entire
+ * notice appears in all copies of the software, derivative works and
+ * supporting documentation.  Further, UCAR requests that the user credit
+ * UCAR/Unidata in any publications that result from the use of this
+ * software or in any product that includes this software. The names UCAR
+ * and/or Unidata, however, may not be used in any advertising or publicity
+ * to endorse or promote any products or commercial entity unless specific
+ * written permission is obtained from UCAR/Unidata. The user also
+ * understands that UCAR/Unidata is not obligated to provide the user with
+ * any support, consulting, training or assistance of any kind with regard
+ * to the use, operation and performance of this software nor to provide
+ * the user with any updates, revisions, new versions or "bug fixes."
+ *
+ * THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package ucar.thredds.catalog.straightimpl;
+
+import thredds.catalog.ServiceType;
+import thredds.catalog2.Catalog;
+import thredds.catalog2.DatasetNode;
+import thredds.catalog2.Property;
+import thredds.catalog2.builder.*;
+import thredds.catalog2.straightimpl.*;
+import ucar.nc2.units.DateType;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * _more_
+ *
+ * @author edavis
+ * @since 4.0
+ */
+class CatalogBuilderImpl implements CatalogBuilder
+{
+  private String name;
+  private String docBaseUri;
+  private String version;
+  private DateType expires;
+  private DateType lastModified;
+
+  private ServiceBuilderContainer serviceBuilderContainer;
+  private CatalogWideServiceBuilderTracker catalogWideServiceBuilderTracker;
+
+  private DatasetNodeContainer datasetContainer;
+
+  private PropertyBuilderContainer propertyBuilderContainer;
+
+  private BuilderIssues builderIssues;
+  private Buildable isBuildable;
+
+  CatalogBuilderImpl( String name, String docBaseUri, String version, DateType expires, DateType lastModified ) {
+    this.docBaseUri = docBaseUri != null ? docBaseUri : "";
+    this.name = name;
+    this.version = version;
+    this.expires = expires;
+    this.lastModified = lastModified;
+
+    this.catalogWideServiceBuilderTracker = new CatalogWideServiceBuilderTracker();
+    this.serviceBuilderContainer = new ServiceBuilderContainer( catalogWideServiceBuilderTracker);
+
+    //this.datasetContainer = new DatasetNodeContainer( null );
+    this.propertyBuilderContainer = new PropertyBuilderContainer();
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  DatasetNodeContainer getDatasetNodeContainer() {
+    return this.datasetContainer;
+  }
+
+  public void setName( String name ) {
+    this.name = name;
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public String getDocBaseUriAsString() {
+    return this.docBaseUri;
+  }
+
+  public void setDocBaseUri( String docBaseUri ) {
+    this.docBaseUri = docBaseUri != null ? docBaseUri : "";
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  public void setVersion( String version ) {
+    this.version = version;
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  public String getVersion() {
+    return this.version;
+  }
+
+  public void setExpires( DateType expires ) {
+    this.expires = expires;
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  public DateType getExpires() {
+    return this.expires;
+  }
+
+  public void setLastModified( DateType lastModified ) {
+    this.lastModified = lastModified;
+    this.isBuildable = Buildable.DONT_KNOW;
+  }
+
+  public DateType getLastModified() {
+    return this.lastModified;
+  }
+
+  public ServiceBuilder addService( String name, ServiceType type, String baseUri ) {
+    this.isBuildable = Buildable.DONT_KNOW;
+    return this.serviceBuilderContainer.addService( name, type, baseUri );
+  }
+
+  public boolean removeService( ServiceBuilder serviceBuilder ) {
+    if ( serviceBuilder == null )
+      return false;
+
+    this.isBuildable = Buildable.DONT_KNOW;
+    return this.serviceBuilderContainer.removeService( serviceBuilder );
+  }
+
+  public List<ServiceBuilder> getServiceBuilders()
+  {
+    return this.serviceBuilderContainer.getServiceBuilders();
+  }
+
+  public ServiceBuilder getServiceBuilderByName( String name )
+  {
+    return this.serviceBuilderContainer.getServiceBuilderByName( name );
+  }
+
+  public ServiceBuilder findServiceBuilderByNameGlobally( String name ) {
+    return this.catalogWideServiceBuilderTracker.getReferenceableService(name);
+  }
+
+  public void addProperty( String name, String value ) {
+    this.isBuildable = Buildable.DONT_KNOW;
+    this.propertyBuilderContainer.addProperty(name, value);
+  }
+
+  public boolean removeProperty( String name ) {
+    this.isBuildable = Buildable.DONT_KNOW;
+    return this.propertyBuilderContainer.removeProperty( name ) != null;
+  }
+
+  public List<String> getPropertyNames() {
+    return this.propertyBuilderContainer.getPropertyNames();
+  }
+
+  public String getPropertyValue( String name ) {
+    return this.propertyBuilderContainer.getPropertyValue( name );
+  }
+
+  public List<Property> getProperties() {
+    return this.propertyBuilderContainer.getProperties();
+  }
+
+  public Property getPropertyByName( String name ) {
+    return this.propertyBuilderContainer.getProperty(name);
+  }
+
+  public DatasetBuilder addDataset( String name ) {
+    this.isBuildable = Buildable.DONT_KNOW;
+//    DatasetImpl di = new DatasetImpl( name, this, null );
+//    this.datasetContainer.addDatasetNode( di );
+//    return di;
+    return null;
+  }
+
+  public CatalogRefBuilder addCatalogRef( String name, String reference ) {
+    this.isBuildable = Buildable.DONT_KNOW;
+//    CatalogRefImpl crb = new CatalogRefImpl( name, reference, this, null );
+//    this.datasetContainer.addDatasetNode( crb );
+//    return crb;
+    return null;
+  }
+
+  public boolean removeDataset( DatasetNodeBuilder builder ) {
+    if ( builder == null )
+      throw new IllegalArgumentException( "DatasetNodeBuilder may not be null.");
+
+    this.isBuildable = Buildable.DONT_KNOW;
+    return this.datasetContainer.removeDatasetNode( (DatasetNodeImpl) builder );
+  }
+
+  public List<DatasetNode> getDatasets() {
+    return this.datasetContainer.getDatasets();
+  }
+
+  public DatasetNode getDatasetById( String id ) {
+    return this.datasetContainer.getDatasetById( id );
+  }
+
+  public DatasetNode findDatasetByIdGlobally( String id ) {
+    return this.datasetContainer.getDatasetNodeByGloballyUniqueId( id );
+  }
+
+  public List<DatasetNodeBuilder> getDatasetNodeBuilders() {
+    return this.datasetContainer.getDatasetNodeBuilders();
+  }
+
+  public DatasetNodeBuilder getDatasetNodeBuilderById( String id ) {
+    return this.datasetContainer.getDatasetNodeBuilderById( id);
+  }
+
+  public DatasetNodeBuilder findDatasetNodeBuilderByIdGlobally( String id ) {
+    return this.datasetContainer.getDatasetNodeByGloballyUniqueId( id );
+  }
+
+  public Buildable isBuildable() {
+    return this.isBuildable;
+  }
+
+  public BuilderIssues checkForIssues()
+  {
+    builderIssues = new BuilderIssues();
+
+    // Check that the baseUri is a valid URI.
+    URI tstDocBaseUri = null;
+    try {
+      tstDocBaseUri = new URI( this.docBaseUri == null ? "" : this.docBaseUri );
+    } catch (URISyntaxException e) {
+      builderIssues.addIssue( new BuilderIssue( BuilderIssue.Severity.ERROR, String.format( "The document base URI [%s] must be a valid URI.", this.docBaseUri), this));
+    }
+    if ( tstDocBaseUri == null || ! tstDocBaseUri.isAbsolute())
+      builderIssues.addIssue( new BuilderIssue( BuilderIssue.Severity.WARNING, String.format( "The document base URI [%s} is not absolute.", this.docBaseUri), this));
+
+    // Check subordinates.
+    builderIssues.addAllIssues( this.serviceBuilderContainer.checkForIssues());
+    builderIssues.addAllIssues( this.propertyBuilderContainer.checkForIssues());
+    builderIssues.addAllIssues( this.catalogWideServiceBuilderTracker.checkForIssues());
+
+    if ( builderIssues.isValid())
+      this.isBuildable = Buildable.YES;
+    else
+      this.isBuildable = Buildable.NO;
+
+    return builderIssues;
+  }
+
+  public Catalog build() throws IllegalStateException
+  {
+    if ( this.isBuildable != Buildable.YES )
+      throw new IllegalStateException( "CatalogBuilder not buildable.");
+
+    return new CatalogImpl( this.name, this.docBaseUri, this.version, this.expires, this.lastModified,
+        this.propertyBuilderContainer, this.serviceBuilderContainer,
+        this.catalogWideServiceBuilderTracker, this.builderIssues );
+  }
+}
---
+++ clcommon/src/main/java/ucar/thredds/catalog/straightimpl/CatalogImpl.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 1998-2009 University Corporation for Atmospheric Research/Unidata
+ *
+ * Portions of this software were developed by the Unidata Program at the
+ * University Corporation for Atmospheric Research.
+ *
+ * Access and use of this software shall impose the following obligations
+ * and understandings on the user. The user is granted the right, without
+ * any fee or cost, to use, copy, modify, alter, enhance and distribute
+ * this software, and any derivative works thereof, and its supporting
+ * documentation for any purpose whatsoever, provided that this entire
+ * notice appears in all copies of the software, derivative works and
+ * supporting documentation.  Further, UCAR requests that the user credit
+ * UCAR/Unidata in any publications that result from the use of this
+ * software or in any product that includes this software. The names UCAR
+ * and/or Unidata, however, may not be used in any advertising or publicity
+ * to endorse or promote any products or commercial entity unless specific
+ * written permission is obtained from UCAR/Unidata. The user also
+ * understands that UCAR/Unidata is not obligated to provide the user with
+ * any support, consulting, training or assistance of any kind with regard
+ * to the use, operation and performance of this software nor to provide
+ * the user with any updates, revisions, new versions or "bug fixes."
+ *
+ * THIS SOFTWARE IS PROVIDED BY UCAR/UNIDATA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UCAR/UNIDATA BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package ucar.thredds.catalog.straightimpl;
+
+import thredds.catalog2.*;
+import thredds.catalog2.builder.BuilderIssues;
+import thredds.catalog2.builder.ThreddsBuilder;
+import thredds.catalog2.straightimpl.*;
+import ucar.nc2.units.DateType;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * _more_
+ *
+ * @author edavis
+ * @since 4.0
+ */
+class CatalogImpl implements Catalog
+{
+  private final String name;
+  private final URI docBaseUri;
+  private final String version;
+  private final DateType expires; // DateType instances are mutable
+  private final DateType lastModified;
+
+  private final ServiceContainer serviceContainer;
+  private final CatalogWideServiceTracker catalogWideServiceTracker;
+
+  //private final DatasetNodeContainer datasetNodeContainer;
+
+  private final PropertyContainer propertyContainer;
+
+  private final ThreddsCatalogIssueContainer threddsCatalogIssueContainer;
+
+
+  CatalogImpl( String name, String docBaseUri, String version, DateType expires, DateType lastModified,
+               PropertyBuilderContainer propertyBuilderContainer, ServiceBuilderContainer serviceBuilderContainer,
+               CatalogWideServiceBuilderTracker catalogWideServiceBuilderTracker, BuilderIssues builderIssues )
+  {
+    if ( propertyBuilderContainer.isBuildable() != ThreddsBuilder.Buildable.YES )
+      throw new IllegalArgumentException( "Failed to build Catalog, PropertyBuilderContainer is not buildable.");
+    if ( serviceBuilderContainer.isBuildable() != ThreddsBuilder.Buildable.YES )
+      throw new IllegalArgumentException( "Failed to build Catalog, ServiceBuilderContainer is not buildable.");
+    if ( catalogWideServiceBuilderTracker.isBuildable() != ThreddsBuilder.Buildable.YES)
+      throw new IllegalArgumentException( "Failed to build Catalog, CatalogWideServiceBuilderTracker is not buildable.");
+//    if ( datasetNodeBuilderContainer.isBuildable() != ThreddsBuilder.Buildable.YES )
+//      throw new IllegalArgumentException( "Failed to build Catalog, DatasetNodeBuilderContainer is not buildable.");
+
+
+    this.name = name == null ? "" : name;
+    try {
+      this.docBaseUri = new URI( docBaseUri == null ? "" : docBaseUri);
+    } catch (URISyntaxException e) {
+      throw new IllegalArgumentException( String.format( "Failed to build Catalog, document base URI [%s] must be valid URI.", docBaseUri));
+    }
+    this.version = version == null ? "" : version;
+    this.expires = expires;
+    this.lastModified = lastModified;
+
+    this.propertyContainer = propertyBuilderContainer.build();
+    this.serviceContainer = serviceBuilderContainer.build();
+    this.catalogWideServiceTracker = catalogWideServiceBuilderTracker.build();
+
+    //this.datasetNodeContainer = datasetNodeBuilderContainer.build();
+
+    this.threddsCatalogIssueContainer = new ThreddsCatalogIssuesImpl( builderIssues);
+  }
+
+//  DatasetNodeContainer getDatasetNodeContainer() {
+//    return this.datasetNodeContainer;
+//  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public URI getDocBaseUri() {
+    return this.docBaseUri;
+  }
+
+  public String getVersion() {
+    return this.version;
+  }
+
+  public DateType getExpires() {
+    return this.expires;
+  }
+
+  public DateType getLastModified() {
+    return this.lastModified;
+  }
+
+  public List<Service> getServices() {
+    return this.serviceContainer.getServices();
+  }
+
+  public Service getServiceByName( String name ) {
+    return this.serviceContainer.getServiceByName( name );
+  }
+
+  public Service findServiceByNameGlobally( String name ) {
+    return this.catalogWideServiceTracker.getServiceByGloballyUniqueName( name );
+  }
+
+  public List<String> getPropertyNames() {
+    return this.propertyContainer.getPropertyNames();
+  }
+
+  public List<Property> getProperties() {
+    return this.propertyContainer.getProperties();
+  }
+
+  public Property getPropertyByName( String name ) {
+    return this.propertyContainer.getProperty( name );
+  }
+
+  public List<DatasetNode> getDatasets() {
+    return null; //this.datasetNodeContainer.getDatasets();
+  }
+
+  public DatasetNode getDatasetById( String id ) {
+    return null; //this.datasetNodeContainer.getDatasetById( id );
+  }
+
+  public DatasetNode findDatasetByIdGlobally( String id ) {
+    return null; //this.datasetNodeContainer.getDatasetNodeByGloballyUniqueId( id );
+  }
+
+  @Override
+  public ThreddsCatalogIssueContainer getIssues() {
+    return this.threddsCatalogIssueContainer;
+  }
+}
